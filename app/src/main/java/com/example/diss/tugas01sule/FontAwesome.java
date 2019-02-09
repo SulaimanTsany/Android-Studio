5d485bd33a6e1705db290e6b4b2ec14c70973dcf
SulaimanTsany <sulaiman.tsany@gmail.com>
2019-02-09 18:09:17 +0800

    fontawesome

---
+++ app/src/main/java/com/example/diss/tugas01sule/FontAwesome.java
@@ -0,0 +1,35 @@
+package com.example.diss.tugas01sule;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class FontAwesome extends AppCompatTextView {
+
+
+    public FontAwesome(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public FontAwesome(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public FontAwesome(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+        //Font name should not contain "/".
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                "fonts/fontawesome-webfont.ttf");
+        setTypeface(tf);
+    }
+
+}
