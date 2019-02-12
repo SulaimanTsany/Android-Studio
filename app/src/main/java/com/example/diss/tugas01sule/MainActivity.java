52ae1ece62844e821cecabfc27cc38e9f34b5ba0
SulaimanTsany <sulaiman.tsany@gmail.com>
2019-02-12 15:49:12 +0800

    hampir selesai

--- app/src/main/java/com/example/diss/tugas01sule/MainActivity.java
+++ app/src/main/java/com/example/diss/tugas01sule/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.diss.tugas01sule;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView) findViewById(R.id.textView2);
-        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
-        tv.setTypeface(tf);
+
+        final Button button = findViewById(R.id.edit_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onClick(View v) {
+                // random a color
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255));
+                button.setBackgroundColor(color);
+            }
+        });
     }
 }
 
