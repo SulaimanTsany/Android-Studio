cad0dff71c854038fe6d1d8cbaf664b5c3c69661
SulaimanTsany <sulaiman.tsany@gmail.com>
2019-02-25 18:49:22 +0800

    bundle bermasalah

--- app/src/main/java/com/example/diss/tugas01sule/InputActivity.java
+++ app/src/main/java/com/example/diss/tugas01sule/InputActivity.java
@@ -4,19 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class InputActivity extends AppCompatActivity {
-
+    TextView name;
+    TextView nim;
+    TextView address;
+    TextView phone;
+    TextView email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        name = findViewById(R.id.input_name);
+        nim = findViewById(R.id.input_nim);
+        address = findViewById(R.id.input_address);
+        phone = findViewById(R.id.input_phone);
+        email = findViewById(R.id.input_email);
     }
 
     public void saveButton (View view) {
         Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name", name.getText());
+        intent.putExtra("nim", nim.getText());
+        intent.putExtra("address", address.getText());
+        intent.putExtra("phone", phone.getText());
+        intent.putExtra("email", email.getText());
         startActivity(intent);
         this.finish();
     }
--- app/src/main/java/com/example/diss/tugas01sule/MainActivity.java
+++ app/src/main/java/com/example/diss/tugas01sule/MainActivity.java
@@ -17,11 +17,36 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name;
+    TextView nim;
+    TextView address;
+    TextView phone;
+    TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        nim = findViewById(R.id.nim);
+        address = findViewById(R.id.address);
+        phone = findViewById(R.id.phone);
+        email = findViewById(R.id.email);
+
+//        Bundle bundle = getIntent().getExtras();
+//        name.setText(bundle.getString("name", getString(R.string.name)));
+//        nim.setText(bundle.getString("nim", getString(R.string.student_id)));
+//        address.setText(bundle.getString("address", getString(R.string.address)));
+//        phone.setText(bundle.getString("phone", getString(R.string.phone_number)));
+//        email.setText(bundle.getString("email", getString(R.string.email)));
+
+        Intent intent = getIntent();
+        name.setText(intent.getStringExtra("name"));
+        nim.setText(intent.getStringExtra("nim"));
+        address.setText(intent.getStringExtra("address"));
+        phone.setText(intent.getStringExtra("phone"));
+        email.setText(intent.getStringExtra("email"));
 
 //        final Button button = findViewById(R.id.edit_button);
 
